89d75423d4ad11a7d4b03fd86d04a7b844ebd848
YuliiaKon <u.pabat@gmail.com>
2022-11-14 12:04:53 +0200

    done 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -6,12 +6,18 @@ const counterValue = 0;
 
 const btnDecrement = document.querySelector("[data-action='decrement']");
 const btnIncrement = document.querySelector("[data-action='increment']");
+const value = document.querySelector('#value');
 
 
-btnDecrement.addEventListener('click', event => {
-    console.log(event);
+btnDecrement.addEventListener('click', () => {
+    counterValue -= 1;
+    return counterValue;
 });
 
 btnIncrement.addEventListener('click', () => {
+    counterValue += 1;
+    return counterValue;
+    console.log(counterValue)
 
 });
+
