ef9f5226716de7877a79a03fbe010e64368bb527
YuliiaKon <u.pabat@gmail.com>
2022-11-16 09:30:51 +0200

    done 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,18 @@
+
+
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('button');
+const span = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', chColor);
+
+function chColor(event) {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = body.style.backgroundColor;
+}
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
